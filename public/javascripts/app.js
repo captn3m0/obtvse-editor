807ed7f41c37d71045999a23f2df600860c8bd24
Abhay Rana <capt.n3m0@gmail.com>
2012-06-15 19:02:41 +0530

    Decided on mustache. Template working on header

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,14 @@
 (function(){
+  Sammy.Mustache = Mustache;
   var config_gist_id = 2936166;
   //Fork https://gist.github.com/2936166 to use as a template
   // initialize the application
   var app = Sammy('#container', function() {
     // include a plugin
-    this.use('Jade');
+    //this.use('Mustache');
 
     // define a 'route'
-    this.get('#/', function() {
+/*    this.get('#/', function() {
       // load some data
       this.load('posts.json')
           // render a template
@@ -15,10 +16,17 @@
           // swap the DOM with the new content
           .swap();
     });
+*/
   });
-
+  console.log('Sending Request');
   //Fetch the config details from main gist
-  $.getJSONP('https://api.github.com/gists/'+config_gist_id,function(){
+  var configUrl = 'https://api.github.com/gists/'+config_gist_id;
+  var configUrl = 'gist.json';
+  $.getJSON(configUrl,function(data){
+    console.log(data.files['config.json'].content);
+    var config=JSON.parse(data.files['config.json'].content);
+    console.log(config);
+    $('header').html(Mustache.to_html($('header').html(),config));
     app.run('#/');
   })
 })()
